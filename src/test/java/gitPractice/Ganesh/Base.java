ccb40f17dfdb58a4d0322f15b27be70c11c374bd
ganesh Shriyan <ganesh.shri2u@gmail.com>
2022-10-05 10:29:13 +0530

    asian commit

---
+++ src/test/java/gitPractice/Ganesh/Base.java
@@ -0,0 +1,12 @@
+package gitPractice.Ganesh;
+
+public class Base {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Basefile");
+
+	}
+
+}
---
+++ src/test/java/gitPractice/Ganesh/Product.java
@@ -0,0 +1,11 @@
+package gitPractice.Ganesh;
+
+public class Product {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("product page");
+
+	}
+
+}
